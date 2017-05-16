3b466fe023302cd8a22aece34eff949388b3cf51
dilarakorkmazgit <dilarakorkmaz@ik-mb-air.lan>
2017-05-16 16:51:43 +0300

    commit2

---
+++ NamütenahiApp/MakeYourDesignView.swift
@@ -0,0 +1,36 @@
+
+//
+//  UserProfileView.swift
+//  NamütenahiApp
+//
+//  Created by Dilara Korkmaz on 02/05/2017.
+//  Copyright © 2017 Dilara Korkmaz. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class MakeYourDesignView: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //current user info
+               
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
